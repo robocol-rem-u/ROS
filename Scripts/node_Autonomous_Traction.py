9f1e34c0e87c5dcb6f14069ac8075cadad94960b
Maria Alejandra Escalante <mariaalejandraep@hotmail.com>
2019-06-17 14:37:07 -0500

    se crearon topicos para el nodo autonomous traction

--- Scripts/node_Autonomous_Traction.py
+++ Scripts/node_Autonomous_Traction.py
@@ -2,10 +2,20 @@
 import rospy
 
 
-def node_Autonomous_Traction():
-    rospy.init_node('node_Autonomous_Traction',anonymous=True)
-    
 
+def goal_Callback(param):
+
+def position_Callback(param):
+
+
+def node_Autonomous_Traction():
+    #creacion del nodo
+    rospy.init_node('node_Autonomous_Traction',anonymous=True)
+    #Se subscribe al topico del Goal y al de la posicion
+    rospy.Subscriber ('topic_Goal', goal, goal_Callback)
+    rospy.Subscriber ('topic_Position', position, position_Callback)
+    # publica en el topico traction orders
+    pub_Traction_Orders = rospy.Publisher('topic_Traction_Orders', traction_Orders, queue_size=10)
 
 
 if __name__ == '__main__':
